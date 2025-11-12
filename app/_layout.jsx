88cc985dc392dc4d84922304718b7647efdc578a
Kristopher Adams <retreat896@gmail.com>
2025-11-12 12:22:10 -0600

    added basic ai'd layout and functionality

---
+++ app/_layout.jsx
@@ -0,0 +1,27 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Capture',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="camera" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tracking"
+        options={{
+          title: 'Tracking',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="eye" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
