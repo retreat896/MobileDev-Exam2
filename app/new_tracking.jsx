d2dce4e49aa61b542e33ad3f5005673ece97bd3b
Jacob Malland <mallandj@uwplatt.edu>
2025-11-14 19:22:10 -0600

    Tab indentation

--- app/new_tracking.jsx
+++ app/new_tracking.jsx
@@ -2,10 +2,10 @@ import { PaintStyle, Skia } from '@shopify/react-native-skia';
 import { useEffect } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import {
-    Camera,
-    useCameraDevice,
-    useCameraPermission,
-    useSkiaFrameProcessor,
+	Camera,
+	useCameraDevice,
+	useCameraPermission,
+	useSkiaFrameProcessor,
 } from 'react-native-vision-camera';
 import { useResizePlugin } from 'vision-camera-resize-plugin';
  
@@ -14,46 +14,45 @@ paint.setStyle(PaintStyle.Fill);
 paint.setColor(Skia.Color('lime'));
  
 export function VisionCameraExample() {
-  const device = useCameraDevice('back');
-  const { hasPermission, requestPermission } = useCameraPermission();
+	const device = useCameraDevice('back');
+	const { hasPermission, requestPermission } = useCameraPermission();
  
-  const { resize } = useResizePlugin();
+	const { resize } = useResizePlugin();
  
-  useEffect(() => {
-    requestPermission();
-  }, [requestPermission]);
+	useEffect(() => {
+		requestPermission();
+	}, [requestPermission]);
  
-  const frameProcessor = useSkiaFrameProcessor((frame) => {
-    'worklet';
+	const frameProcessor = useSkiaFrameProcessor((frame) => {
+		'worklet';
  
-    const height = frame.height / 4;
-    const width = frame.width / 4;
+		const height = frame.height / 4;
+		const width = frame.width / 4;
  
-    const resized = resize(frame, {
-      scale: {
-        width: width,
-        height: height,
-      },
-      pixelFormat: 'bgr',
-      dataType: 'uint8',
-    });
+		const resized = resize(frame, {
+			scale: {
+				width: width,
+				height: height,
+			},
+			pixelFormat: 'bgr',
+			dataType: 'uint8',
+		});
+	}, []);
  
-  }, []);
+	if (!hasPermission) {
+		return <Text>No permission</Text>;
+	}
  
-  if (!hasPermission) {
-    return <Text>No permission</Text>;
-  }
+	if (device == null) {
+		return <Text>No device</Text>;
+	}
  
-  if (device == null) {
-    return <Text>No device</Text>;
-  }
- 
-  return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={true}
-      frameProcessor={frameProcessor}
-    />
-  );
+	return (
+		<Camera
+			style={StyleSheet.absoluteFill}
+			device={device}
+			isActive={true}
+			frameProcessor={frameProcessor}
+		/>
+	);
 }
